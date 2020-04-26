e2cddf8ef0b328760733853a97ee2e9ecccd6883
PROX-E <lichlord18@gmail.com>
2020-04-26 14:09:08 -0600

    update read function

--- Develop/public/db.js
+++ Develop/public/db.js
@@ -30,4 +30,25 @@ request.onerror = function(event) {
     const transaction = db.transaction(["pending"], "readwrite");
     const store = transaction.objectStore("pending");
     const getAll = store.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+          fetch("/api/transaction/bulk", {
+            method: "POST",
+            body: JSON.stringify(getAll.result),
+            headers: {
+              Accept: "application/json, text/plain, */*",
+              "Content-Type": "application/json"
+            }
+          })
+          .then(response => response.json())
+        .then(() => {
+          // delete records if successful
+          const transaction = db.transaction(["pending"], "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
+    }
+  };
+        
   };
