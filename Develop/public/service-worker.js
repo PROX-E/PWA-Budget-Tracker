e13be96d1c0a4f9193b89790547c1b9eca2e9f2a
PROX-E <lichlord18@gmail.com>
2020-04-26 13:58:56 -0600

    fetch cached files

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -37,3 +37,29 @@ const FILES_TO_CACHE = [
         );
         self.clients.claim();
     });
+
+    // Fetch Files
+    self.addEventListener('fetch',  evt =>{
+        if (evt.request.url.includes('/api/')) {
+            console.log('[Service Worker] Fetch (data)', evt.request.url);
+
+            evt.respondWith(
+                caches.open(DATA_CACHE_NAME).then(cache => {
+                    return fetch(evt.request)
+                        .then(response  => {
+                            if (response.status === 200) {
+                                cache.put(evt.request.url, response.clone());
+                            }
+                            return response;
+                        })
+                        .catch(err => {
+                            return cache.match(evt.request);
+                        });
+                })
+            );
+
+            return;
+        }
+
+        
+    });
